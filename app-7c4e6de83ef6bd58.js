d68d4b1febdbdd5bd597a8386491834b38744c93
maxerbox <12765875+maxerbox@users.noreply.github.com>
2022-12-15 15:50:14 +0000

    Deploying to gh-pages from @ maxerbox/yew-trunk-web-worker-flowbite-tailwind-rust-wasm-example-app@3e3a04b6aa9fbc3fadca16b3775ea6881a7bccad 🚀

--- app-7c4e6de83ef6bd58.js
+++ app-7c4e6de83ef6bd58.js
@@ -695,7 +695,7 @@ function initSync(module) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('app-aa1ced51bb2303e_bg.wasm', import.meta.url);
+        input = new URL('app-7c4e6de83ef6bd58_bg.wasm', import.meta.url);
     }
     const imports = getImports();
 
