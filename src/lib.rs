87385ef525bd16320a327d6c0de92786dcb36a7d
Simon Sassi <2765875+maxerbox@users.noreply.github.com>
2022-12-15 16:34:20 +0100

    feat: add footer

--- src/lib.rs
+++ src/lib.rs
@@ -23,13 +23,26 @@ pub fn app() -> Html {
         }))
     });
     html! {
+        <>
+        <main>
     <section class="h-screen dark:bg-gray-900 grid-cols-3 grid-rows-3 grid">
-         <div class="py-8 px-4 my-auto mx-auto max-w-screen-md text-center lg:py-16 lg:px-12 row-start-2 col-start-2">
+         <div class="pb-8 px-4 my-auto mx-auto max-w-screen-md text-center lg:pb-16 lg:px-12 row-start-2 col-start-2">
             <FileDropper on_file_input={Callback::from(move |content: String| {
                worker.borrow_mut().send(SGMLTransformWorkerInput{ file: content });
                 })}
              />
         </div>
      </section>
+     </main>
+         <footer
+           class="p-4 bg-white shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 absolute bottom-0 w-screen"
+         >
+           <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400"
+             >{"© 2022 "}
+             <a href="https://github.com/maxerbox/yew-trunk-web-worker-flowbite-tailwind-rust-wasm-example-app" class="hover:underline">{"GitHub"}</a> {" - Simon Sassi - Tranforms OFX files for LBP"}
+           </span>
+           <script src="https://unpkg.com/flowbite@1.5.4/dist/flowbite.js"></script>
+         </footer>
+         </>
     }
 }
